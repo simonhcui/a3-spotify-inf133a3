5b7030bea904a000ad1654a0e5566cb7cb077860
simonhcui <simonhcui@gmail.com>
2018-11-13 19:56:36 -0800

    Add files via upload

--- client/src/app/pages/track-page/track-page.component.ts
+++ client/src/app/pages/track-page/track-page.component.ts
@@ -4,6 +4,8 @@ import { ArtistData } from '../../data/artist-data';
 import { TrackData } from '../../data/track-data';
 import { AlbumData } from '../../data/album-data';
 import { TrackFeatures } from '../../data/track-features';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-track-page',
@@ -15,11 +17,34 @@ export class TrackPageComponent implements OnInit {
 	track:TrackData;
   audioFeatures:TrackFeatures;
   featureTypes = TrackFeatures.FeatureTypes;
+  artistname:string;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private http:HttpClient, private route: ActivatedRoute) { }
 
   ngOnInit() {
-  	this.trackId = this.route.snapshot.paramMap.get('id');
+    this.trackId = this.route.snapshot.paramMap.get('id');
+    
+    this.http.get('http://localhost:8888/track/' 
+    + this.trackId + '/' ).toPromise().then(response => response)
+    .then (data => {
+//      console.log(data);
+      this.track = new TrackData(data);
+      for(let i=0; i<data["artists"].length; i++){
+        //console.log(data["artists"][i]);
+        //(this.topTracks).push(new TrackData((data["tracks"])[i]));
+    }
+      this.artistname = data["artists"][0].name;
+    });
+
+    this.http.get('http://localhost:8888/track-audio-features/' 
+    + this.trackId + '/' ).toPromise().then(response => response)
+    .then (data => {
+//      console.log(data);
+
+      this.audioFeatures = new TrackFeatures(data);
+      //this.featureTypes = this.audioFeatures.f
+      //console.log(this.audioFeatures);
+    });
   	//TODO: Inject the spotifyService and use it to get the track data and it's audio features
   }
 
