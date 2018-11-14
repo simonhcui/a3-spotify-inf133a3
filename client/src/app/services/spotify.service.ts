473c69d2fcd7e035b5c871a91a2b8ab66bd6ed3b
simonhcui <simonhcui@gmail.com>
2018-11-13 19:51:21 -0800

    Add files via upload

--- client/src/app/services/spotify.service.ts
+++ client/src/app/services/spotify.service.ts
@@ -10,11 +10,16 @@ export class SpotifyService {
   constructor(private http:HttpClient) { }
 
   private sendRequestToExpress(endpoint:string):Promise<any> {
+    
     //TODO: use the injected http Service to make a get request to the Express endpoint and return the response.
     //the http service works similarly to fetch(). It may be useful to call .toPromise() on any responses.
     //update the return to instead return a Promise with the data from the Express server
     return Promise.resolve();
   }
 
+  myme():Promise<any> {
+    this.http.get( this.expressBaseUrl +'/me').toPromise();
+    return Promise.resolve();
+    }
   //TODO: create functions for all of the endpoints on the webserver (except for /login, which is linked to from a button in the navbar).
 }
