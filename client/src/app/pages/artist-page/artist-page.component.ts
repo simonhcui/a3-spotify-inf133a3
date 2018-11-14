b91c4da9ee3024c9dfdeee7b5c15e476771e21c7
simonhcui <simonhcui@gmail.com>
2018-11-13 19:54:11 -0800

    Add files via upload

--- client/src/app/pages/artist-page/artist-page.component.ts
+++ client/src/app/pages/artist-page/artist-page.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { ArtistData } from '../../data/artist-data';
 import { TrackData } from '../../data/track-data';
 import { AlbumData } from '../../data/album-data';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-artist-page',
@@ -14,13 +15,57 @@ export class ArtistPageComponent implements OnInit {
 	artist:ArtistData;
 	relatedArtists:ArtistData[];
 	topTracks:TrackData[];
-	albums:AlbumData[];
+  albums:AlbumData[];
+  img:string;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private http:HttpClient, private route: ActivatedRoute) { }
 
   ngOnInit() {
-  	this.artistId = this.route.snapshot.paramMap.get('id');
-    //TODO: Inject the spotifyService and use it to get the artist data, related artists, top tracks for the artist, and the artist's albums
+    this.artistId = this.route.snapshot.paramMap.get('id');
+    this.topTracks = []; 
+    this.albums =[];
+    this.relatedArtists = [];
+
+    this.http.get('http://localhost:8888/artist/' 
+    + this.artistId + '/' ).toPromise().then(response => response)
+    .then (data => {
+      //console.log(data);
+      this.artist = new ArtistData(data);
+      this.img = this.artist.imageURL;
+      //console.log(this.artist.url);
+  });
+
+  this.http.get('http://localhost:8888/artist-top-tracks/' 
+  + this.artistId + '/' ).toPromise().then(response => response)
+  .then (data => {
+//    console.log(data);
+  for(let i=0; i<(data["tracks"]).length; i++){
+      console.log(data["tracks"][i]);
+      (this.topTracks).push(new TrackData((data["tracks"])[i]));
+    }
+  });
+
+  this.http.get('http://localhost:8888/artist-albums/' 
+  + this.artistId + '/' ).toPromise().then(response => response)
+  .then (data => {
+//    console.log(data);
+    for(let i=0; i<(data["items"]).length; i++){
+      console.log(data["items"][i]);
+      (this.albums).push(new AlbumData((data["items"])[i]));
+    }
+  });
+
+  this.http.get('http://localhost:8888/artist-related-artists/' 
+  + this.artistId + '/' ).toPromise().then(response => response)
+  .then (data => {
+//    console.log(data);
+    for(let i=0; i<(data["artists"]).length; i++){
+      console.log(data["artists"][i]);
+      (this.relatedArtists).push(new ArtistData((data["artists"])[i]));
+    }
+  });
+
+  //TODO: Inject the spotifyService and use it to get the artist data, related artists, top tracks for the artist, and the artist's albums
   }
 
 }
